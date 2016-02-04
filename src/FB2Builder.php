d16949f92f678a1fa00396cad564f96a69652d21
head26 <26soft@mail.ru>
2016-02-04 16:02:36 +0300

    add object

--- src/FB2Builder.php
+++ src/FB2Builder.php
@@ -13,40 +13,32 @@ if (!defined('FB2Builder_ROOT')) {
     require(FB2Builder_ROOT . 'Autoloader.php');
 }
 
+/**
+ * Class FB2Builder
+ * @package FB2Builder
+ */
 class FB2Builder
 {
+    /**
+     * @var null
+     */
     protected $fictionBook = NULL;
-    protected $description = NULL;
-    protected $body = NULL;
-    protected $stylesheet = NULL;
-    protected $binary = NULL;
 
 
     /**
-     * @return \FB2BuilderDescription|null
+     * @return \FB2BuilderFictionBook|null
      */
-    public function getDescription()
+    public function getFictionBook()
     {
-        if(!$this->description instanceof \FB2BuilderDescription)
-            return $this->description = new \FB2BuilderDescription();
-        return $this->description;
+        if(!$this->fictionBook instanceof \FB2BuilderFictionBook)
+            return $this->fictionBook = new \FB2BuilderFictionBook();
+        return $this->fictionBook;
     }
-
-
-    /**
-     * @return \FB2BuilderDescription|null
-     */
-    public function getBody()
-    {
-        if(!$this->description instanceof \FB2BuilderDescription)
-            return $this->description = new \FB2BuilderDescription();
-        return $this->description;
-    }
-
 
 }
 $a = new FB2Builder();
-$titleInfo = $a->getDescription()->getTitleInfo();
+$fb = $a->getFictionBook();
+$titleInfo = $fb->getDescription()->getTitleInfo();
 $titleInfo->getAuthor()->setFirstname('Имя');
 $titleInfo->getAuthor()->setLastname('ФАм');
 $titleInfo->getAuthor()->setNickname('head');
@@ -55,8 +47,9 @@ $titleInfo->getDate()->setDate('04.02.2016');
 $titleInfo->getLang()->setLang('ru');
 $titleInfo->getLang()->getValue();
 
-$genres = ['sf_cyberpunk','sf_space','sf'];
 $genre = $titleInfo->getGenre();
+$genres = ['sf_cyberpunk','sf_space','sf'];
+
 foreach($genres as $k => $v)
     $genre->add($v);
 
---
+++ src/nodes/FictionBook.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Denis Kuliev
+ * Date: 04.02.2016
+ * Time: 15:59
+ */
+class FB2BuilderFictionBook
+{
+    protected $description = NULL;
+    protected $body = NULL;
+    protected $stylesheet = NULL;
+    protected $binary = NULL;
+
+    /**
+     * @return FB2BuilderDescription|null
+     */
+    public function getDescription()
+    {
+        if(!$this->description instanceof FB2BuilderDescription)
+            return $this->description = new FB2BuilderDescription();
+        return $this->description;
+    }
+
+
+    /**
+     * @return FB2BuilderDescription|null
+     */
+    public function getBody()
+    {
+        if(!$this->description instanceof FB2BuilderDescription)
+            return $this->description = new FB2BuilderDescription();
+        return $this->description;
+    }
+}
