15920392e0cdd1c998628f0dbf74bc23b5dc0690
head26 <26soft@mail.ru>
2016-03-01 17:29:17 +0300

    add change

--- src/nodes/Publisher.php
+++ src/nodes/Publisher.php
@@ -13,7 +13,38 @@ namespace FB2Builder\nodes;
  * Class Publisher
  * @package FB2Builder\nodes
  */
-class Publisher
+class Publisher extends Author
 {
-// TODO: описать класс. В зависимости от родителя разное поведение.
+    /**
+     * @var string
+     */
+    protected $value = '';
+
+
+    /**
+     * Set value
+     * @param $value
+     * @return $this
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+    /**
+     * Get value
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+    /**
+     * XML Node Name
+     * @return string
+     */
+    static public function getXMLNodeName()
+    {
+        return 'publisher';
+    }
 }
